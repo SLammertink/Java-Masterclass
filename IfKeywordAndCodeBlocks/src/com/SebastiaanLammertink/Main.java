a1c30a10e30141b9ae924ba0991696be46f13d65
SLammertink <Bea991213!>
2020-11-03 14:48:00 +0100

    November 3 2020

--- IfKeywordAndCodeBlocks/src/com/SebastiaanLammertink/Main.java
+++ IfKeywordAndCodeBlocks/src/com/SebastiaanLammertink/Main.java
@@ -9,6 +9,33 @@ public class Main {
         System.out.println("Your final score was: " + calculateScore(true, 800, 5, 100));
         System.out.println("Your final score was: " + calculateScore(true, 10000, 8, 200));
 
+        int highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Louise", highScorePosition);
+        highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Bob", highScorePosition);
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Maria", highScorePosition);
+        highScorePosition = calculateHighScorePosition(250);
+        displayHighScorePosition("Jorien", highScorePosition);
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Jan", highScorePosition);
+    }
+
+    public static void displayHighScorePosition( String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into position "
+        + highScorePosition + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore >= 1000) {
+            return 1;
+        }else if (playerScore >= 500 ) {
+            return 2;
+        }else if (playerScore >= 100 ){
+            return 3;
+        }else {
+            return 4;
+        }
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
--- SpeedConverter/src/SpeedConverter.java
+++ SpeedConverter/src/SpeedConverter.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class SpeedConverter {
+public class SpeedConverter {
+
+    public static long toMilesPerHour(double kiloMetersPerHour) {
+        if (kiloMetersPerHour < 0) {
+            return -1;
+        }
+        return Math.round(kiloMetersPerHour / 1.609);
+    }
+
+    public static long printConversion(double kilometersPerHour) {
+
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h" + " = " + milesPerHour + " mi/h");
+        }
+        return -1;
+
+    }
 }
